18195b05ff3c8c13818534f968b54319cbe03d68
mochfatchur <mofan13519009@gmail.com>
2025-01-05 21:29:04 +0700

    fix: sequelize cli config

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT || 5432, // Default PostgreSQL port
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT || 5432,
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT || 5432,
+    },
+};
--- models/index.js
+++ models/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
-const { config } = require(__dirname + '/../config/dbConfig.js');
+const env = process.env.NODE_ENV || 'development'; // Default ke 'development'
+const config = require(__dirname + '/../config/config.js')[env]; // Pilih config sesuai environment
 const db = {};
 
 
